1e65c2d98f26ba97f915bfb8a116437c9d3a1735
Mycents <prakashsubramani@hotmail.com>
2020-01-06 15:46:10 -0500

    Experiment with relationship between 2 type definitions and use custom resolvers for the relationship

--- src/index.js
+++ src/index.js
@@ -11,21 +11,45 @@ const schema = gql`
         users: [User!]
         me: User
         user(id: ID!): User
+        messages: [Message!]!
+        message(id: ID!): Message!
     }
 
     type User {
         id: ID!
         username: String!
+        messages: [Message!]
+    }
+
+    type Message {
+        id: ID!
+        text: String!
+        user: User!
     }
 `
 let users = {
     1: {
         id: '1',
         username: 'Prakash V',
+        messageIds: [1]
     },
     2: {
         id: '2',
-        username: 'Anika'
+        username: 'Anika',
+        messageIds: [2]
+    },
+};
+
+let messages = {
+    1: {
+        id: '1',
+        text: 'Hello World',
+        userId: '1'
+    },
+    2: {
+        id: '2',
+        text: 'By World',
+        userId: '2'
     },
 };
 
@@ -42,11 +66,29 @@ const resolvers = {
         users:() => {
             return Object.values(users);
         },
+
+        messages:() => {
+            return Object.values(messages);
+        },
+
+        message:(parent, {id}) => {
+            return messages[id];
+        },
     },
 
     User: {
-        username: user => user.username
+       messages: user => {
+           return Object.values(messages).filter(
+               message => message.userId === user.id
+           )
+       },
     },
+
+    Message: {
+        user: message => {
+            return users[message.userId];
+        }
+    }
 };
 
 const server = new ApolloServer({
