4449b2c57f192af77c0bdd5e55d0aff95b2c0f6f
Mycents <prakashsubramani@hotmail.com>
2020-01-07 16:11:55 -0500

    Add mutations and mutation resolvers

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "dotenv/config"
 import cors from "cors"
 import express from "express"
 import {ApolloServer, gql} from "apollo-server-express"
+import uuidv4 from 'uuid/v4';
 
 const app = express()
 app.use(cors())
@@ -13,6 +14,11 @@ const schema = gql`
         user(id: ID!): User
         messages: [Message!]!
         message(id: ID!): Message!
+    }
+
+    type Mutation {
+        createMessage(text: String!): Message!
+        deleteMessage(id: ID!): Boolean!
     }
 
     type User {
@@ -74,6 +80,34 @@ const resolvers = {
         message:(parent, {id}) => {
             return messages[id];
         },
+    },
+
+    Mutation: {
+        createMessage: (parent, {text}, {me}) => {
+            const id = uuidv4();
+            const message = {
+                id,
+                text,
+                userId: me.id,
+            };
+
+            messages[id] = message;
+            users[me.id].messageIds.push(id);
+            return message;
+        },
+        deleteMessage: (parent, {id}) => {
+            console.log(messages)
+            const {[id]: message, ...otherMessages} = messages;
+            console.log("message is", message)
+            console.log("Other message is", otherMessages)
+            if(!message) {
+                return false;
+            }
+
+            messages = otherMessages;
+
+            return true;
+        }
     },
 
     User: {
